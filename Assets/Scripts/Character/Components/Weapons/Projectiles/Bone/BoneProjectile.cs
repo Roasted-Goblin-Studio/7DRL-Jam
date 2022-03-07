b981e6eb80e13995b78ee0ea25f4d9fd86f4d389
Weston Vincze <westonvincze@gmail.com>
2022-03-07 08:27:57 -0500

    Character/ranged revamp (#45)
    * Slight cleanup on meleeAttack and HP
    
    * Simplifies projectile collision logic
    
    * Allows projectiles to deal damage to any "enemy" with Health instead of only characters
    
    * Adds self damage to bone throw
    
    * Adds death animation to player

--- Assets/Scripts/Character/Components/Weapons/Projectiles/Bone/BoneProjectile.cs
+++ Assets/Scripts/Character/Components/Weapons/Projectiles/Bone/BoneProjectile.cs
@@ -8,25 +8,4 @@ public class BoneProjectile : Projectile
         _ProjectileMovement = Direction * _Speed * Time.deltaTime;
         _ProjectileRigidBody2D.MovePosition(_ProjectileRigidBody2D.position + _ProjectileMovement);
     }
-
-    protected override void OnTriggerEnter2D(Collider2D other)
-    {
-        if(other.tag == "Enemy"){
-            CharacterHealth characterHealth = other.GetComponent<CharacterHealth>();
-            characterHealth.Damage(_ProjectileDamage);
-            _ReturnObjectToPool.DestroyObject();
-        }
-        
-        foreach (var tag in _TagsToAvoid.TagsToAvoidStrings)
-        {
-            if(other.tag == tag){
-                return;
-            }
-        }
-        if(other.tag == "Non Hitable") {
-            return;
-        }
-
-        _ReturnObjectToPool.DestroyObject();
-    }
 }
