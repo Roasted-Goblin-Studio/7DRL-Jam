7ddaa3845bcc82cb9d8dc5140bce7c16e6a19071
Weston Vincze <westonvincze@gmail.com>
2022-03-06 19:52:13 -0500

    Finalizes melee attack logic and death logic

--- Assets/Scripts/Character/Components/Defaults/CharacterHealth.cs
+++ Assets/Scripts/Character/Components/Defaults/CharacterHealth.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class CharacterHealth : Health
 {
+    private Animator _Animator;
+
     protected override void Start(){
         base.Start();
         _Character = GetComponent<Character>();
+        _Animator = GetComponentInChildren<Animator>();
+    }
+
+    public override void Die()
+    {
+        base.Die();
+
+        if (!_Animator) return;
+
+        _Animator.SetTrigger("die");
     }
 }
