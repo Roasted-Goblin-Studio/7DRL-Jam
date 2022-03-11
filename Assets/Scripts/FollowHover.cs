24c7ccd8ea5efed806a313f255154dacc6534531
forager-pixel <forager.pixel@gmail.com>
2022-03-11 17:15:39 -0500

    Remove coroutine for follow
    - Coroutine too janky, updates poorly when mouse hovers over multiple buttons in quick succession

--- Assets/Scripts/FollowHover.cs
+++ Assets/Scripts/FollowHover.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class FollowHover : MonoBehaviour
 {
-    [SerializeField] private float lerpDuration = .5f; 
+    public float speed = 1.0f;
+
+    [SerializeField] private Transform startTransform;
+    private Vector3 targetPos;
 
     void OnEnable() {
         MenuButton.onMenuButtonHover += ChangePosition;
@@ -29,19 +32,29 @@ public class FollowHover : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {   
+        targetPos = new Vector3(transform.position.x, startTransform.position.y, transform.position.z);
+        transform.position = targetPos;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Move our position a step closer to the target.
+        float step =  speed * Time.deltaTime; // calculate distance to move
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, step);
+         // Check if the position of the cube and sphere are approximately equal.
+        if (Vector3.Distance(transform.position, targetPos) < 0.001f)
+        {
+            // Swap the position of the cylinder.
+            //target.position *= -1.0f;
+        }
     }
 
     void ChangePosition(MenuButton button) {
-        StopCoroutine("Lerp");
-        Debug.Log("Changing position!");
-        StartCoroutine(Lerp(button.transform));
+        targetPos = new Vector3(transform.position.x, button.transform.position.y, transform.position.z);
+        //StopCoroutine("Lerp");
+        //Debug.Log("Changing position!");
+        //StartCoroutine(Lerp(button.transform));
     }
 }
