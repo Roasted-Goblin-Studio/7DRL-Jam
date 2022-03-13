9b141bd4299a6b279fecec0155787cc32e1c9dd9
CasVDG <88520965+CasVDG@users.noreply.github.com>
2022-03-13 12:02:40 -0400

    Level Music Added

---
+++ Assets/Scripts/Character/Components/Audio/LevelMusicManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelMusicManager : MonoBehaviour
+{
+    private static FMOD.Studio.EventInstance Music;
+
+    [FMODUnity.EventRef]
+    [SerializeField]
+    private string musicEvent;
+
+    void Start()
+    {
+        Music = FMODUnity.RuntimeManager.CreateInstance(musicEvent);
+        Music.start();
+        Music.release();
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void StopMusic()
+    {
+        Music.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    }
+
+    void OnDestroy()
+    {
+        Music.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    }
+}
