f849e818b3dda2c5ea851811cd4adb7d7055fa33
forager-pixel <99606581+forager-pixel@users.noreply.github.com>
2022-03-13 13:06:23 -0400

    Character/UI (#89)
    * Character Health events
    
    - Add damage and heal events to character health
    - Add glen scene
    
    * Health UI Implementation
    
    - Basic hp bar and animations
    
    * Array bound checks for UI
    
    * Reticle and animation
    
    * Cursor transitions
    
    * Character Health events
    
    - Add damage and heal events to character health
    - Add glen scene
    
    * Health UI Implementation
    
    - Basic hp bar and animations
    
    * Array bound checks for UI
    
    * Reticle and animation
    
    * Cursor transitions
    
    * Update Bone Empty.anim
    
    * Update player prefab

--- Assets/Scripts/Character/Components/Defaults/CharacterHealth.cs
+++ Assets/Scripts/Character/Components/Defaults/CharacterHealth.cs
@@ -6,6 +6,20 @@ public class CharacterHealth : Health
 {
     private Character _Character;
 
+    // Events
+    public delegate void OnDamage(int amount);
+    public static event OnDamage onDamage;
+
+    public delegate void OnHeal(int amount);
+    public static event OnHeal onHeal;
+
+    public delegate void OnMaxHealthIncrease(int amount, bool heal);
+    public static event OnMaxHealthIncrease onMaxHealthIncrease;
+
+    public delegate void OnDeath();
+    public static event OnDeath onDeath;
+    // ******* //
+
     protected override void Start(){
         base.Start();
         _Character = GetComponent<Character>();
@@ -17,11 +31,31 @@ public class CharacterHealth : Health
     {
         if(_Character && !_Character.IsHitable) return;
         base.Damage(amount, playHurtAnim);
+        onDamage?.Invoke((int) amount);
+    }
+
+    public override void IncreaseMaxHealth(float amount)
+    {
+        base.IncreaseMaxHealth(amount);
+        onMaxHealthIncrease?.Invoke((int) amount, false);
+    }
+
+    public override void IncreaseMaxHealthAndHeal(float amount)
+    {
+        base.IncreaseMaxHealth(amount);
+        onMaxHealthIncrease?.Invoke((int) amount, true);
+    }
+
+    public override void Heal(float amount)
+    {
+        base.Heal(amount);
+        onHeal?.Invoke((int) amount);
     }
 
     public override void Die()
     {
         base.Die();
+        onDeath?.Invoke();
 
         _CurrentHealth = 0;
         if (_Character) _Character.IsAlive = false;
